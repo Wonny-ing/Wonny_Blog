17c5b10236ba9ecdde4c69bace187c2981685ce5
Wonny-ing <h.wonny11@gmail.com>
2023-04-26 02:30:33 +0900

    Env: 초기세팅 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-josefin)'],
+        serif: ['var(--font-source-serif-pro)'],
+      },
+      colors: {
+        uPrimary: '#4A4870',
+        uFontColor: '#586171',
+        uBlue: '#4A4870',
+        uRed: '#FF4B60',
+        uWhite: '#FFFFFF',
+        uGray: '#9A9FA9',
+        uTwitter: '#1DA1F1',
+        uFacebook: '#165BEA',
+        uInstargram: '#EF00A1',
+        uYellow: '#FAC469',
+        uSkyBlue: '#65EBE7',
       },
     },
   },
   plugins: [],
-}
+};
